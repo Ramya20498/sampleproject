4e3d29f44114ac16ea8eee0f533c3034e1b46a68
Ramya <Ramyasandra20@gmail.com>
2022-06-13 21:56:17 +0530

    creating a login page

---
+++ src/simpleprogram/Loginpage.java
@@ -0,0 +1,11 @@
+package simpleprogram;
+
+public class Loginpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("creating a new login page");
+
+	}
+
+}
